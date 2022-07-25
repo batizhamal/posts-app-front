ff8e0b3be55875dbe39e5536e8d62a7bbf3ebee0
Batizhamal <52880286+batizhamal@users.noreply.github.com>
2022-07-25 16:40:48 +0600

    table style not finished

--- src/views/Posts/List/block/Table.vue
+++ src/views/Posts/List/block/Table.vue
@@ -1,29 +1,32 @@
 <template>
   <div class="container">
-    <table>
+    <table class="table">
       <thead>
         <tr>
-          <th>№</th>
-          <th>Title</th>
-          <th>Body</th>
-          <th>Actions</th>
+          <th class="table__head">№</th>
+          <th class="table__head">Title</th>
+          <th class="table__head">Body</th>
+          <th class="table__head">Actions</th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="(post, index) in posts" :key="`post-${index}`">
-          <td>{{ post.id }}</td>
-          <td>{{ post.title }}</td>
-          <td>{{ post.body }}</td>
-          <td
+          <td class="table__col">{{ post.id }}</td>
+          <td class="table__col">{{ post.title }}</td>
+          <td class="table__col">{{ post.body }}</td>
+
+          <div
             v-for="(action, actionIndex) in actions(post)"
             :key="`action-${actionIndex}`"
           >
-            <UIButton
-              v-if="action.if()"
-              v-bind="action.props"
-              @click="action.click"
-            />
-          </td>
+            <td class="table__col actions">
+              <UIButton
+                v-if="action.if()"
+                v-bind="action.props"
+                @click="action.click"
+              />
+            </td>
+          </div>
         </tr>
       </tbody>
     </table>
@@ -51,5 +54,35 @@ export default {
   height: calc(100% - $panel_height * 2);
   width: 100%;
   overflow: scroll;
+}
+
+.table {
+  $self: &;
+  position: relative;
+  border-collapse: collapse;
+  table-layout: fixed;
+  width: 100%;
+
+  &__head {
+    position: sticky;
+    top: 0;
+    z-index: 2;
+    height: 3rem;
+    box-shadow: inset 0 -0.5px 0 0 #d4d4d4;
+    text-align: left;
+    background-color: #f2f2f2;
+  }
+
+  &__head,
+  &__col {
+    padding: 0 0.75rem;
+  }
+}
+
+.actions {
+}
+
+tr:nth-child(even) {
+  background-color: #f8f8f8;
 }
 </style>
